45bd4bae5cf2079d391f49db9b95f1b0577a5a8a
Tahirih <tahysluana@gmail.com>
2020-05-09 17:39:39 -0500

    buil(home) export components

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+/*components*/
 import { MoneyExchangeComponent } from './components/money-exchange/money-exchange.component';
 import { MenuListComponent } from './components/menu-list/menu-list.component';
 
-
-
 @NgModule({
-  declarations: [HomeComponent, MoneyExchangeComponent, MenuListComponent],
+  declarations: [
+    MoneyExchangeComponent,
+    MenuListComponent
+  ],
+  exports: [
+    MoneyExchangeComponent,
+    MenuListComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class HomeModule { }
