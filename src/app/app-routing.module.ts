5fa7e793c9f682a58ab6ea3d04d10fe3dd82ed9b
Tahirih <tahysluana@gmail.com>
2020-05-09 16:21:02 -0500

    build(app) add routes for redirect to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+/*Components*/
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    component: HomeComponent
+  },
+  { path: '**', redirectTo: '/inicio'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
