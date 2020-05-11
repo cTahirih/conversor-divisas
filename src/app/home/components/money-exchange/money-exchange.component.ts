4f7d75aed144b10b8010cfc70dc4b4cf9ea659dd
Tahirih <tahysluana@gmail.com>
2020-05-10 19:47:41 -0500

    buil(home) get rate currency fromservice and calculate value in dollars

--- src/app/home/components/money-exchange/money-exchange.component.ts
+++ src/app/home/components/money-exchange/money-exchange.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { HomeService } from '../../services/home.service';
+
 @Component({
   selector: 'app-money-exchange',
   templateUrl: './money-exchange.component.html',
@@ -9,9 +11,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class MoneyExchangeComponent implements OnInit {
 
   moneyExchange: FormGroup;
+  rateExchange: number;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private homeService: HomeService
   ) { }
 
   ngOnInit(): void {
@@ -19,6 +23,17 @@ export class MoneyExchangeComponent implements OnInit {
       soles: [null, Validators.required],
       dollar: [null]
     });
+
+    this.calculateAmount();
+    this.rateExchange = this.homeService.getRateOfExchange();
   }
 
+  calculateAmount() {
+    this.moneyExchange.controls.soles.valueChanges.subscribe((value) => {
+
+      const amount = Number(value) * this.rateExchange;
+      this.moneyExchange.controls.dollar.setValue(amount * 3);
+
+    });
+  }
 }
