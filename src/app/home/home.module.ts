11bae7b3877e27fd5c866d2f095711daa39b1f15
Tahirih <tahysluana@gmail.com>
2020-05-14 15:18:01 -0500

    build(home) import package for currency and implements en input soles

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
 
 /*components*/
 import { MoneyExchangeComponent } from './components/money-exchange/money-exchange.component';
@@ -19,6 +20,7 @@ import { MenuListComponent } from './components/menu-list/menu-list.component';
   imports: [
     CommonModule,
     HttpClientModule,
+    CurrencyMaskModule,
     ReactiveFormsModule
   ]
 })
